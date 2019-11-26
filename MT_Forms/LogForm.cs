80f87517d2ce73938f17d42a210609c74e04893c
Vitalii Seredenko <Vitalii.Seredenko@thomsonreuters.com>
2019-11-26 13:24:16 +0200

    add button text change feature in logForm

--- MT_Forms/LogForm.cs
+++ MT_Forms/LogForm.cs
@@ -19,21 +19,37 @@ namespace MT_Forms
         private void ShowLogButton_Click(object sender, EventArgs e)
         {
             _logger.WriteAllLogInLogBox();
+            showLogButton.Text = "Обновить";
+            showInfoLogButton.Text = "Только INFO";
+            showErrorLogButton.Text = "Только ERROR";
+            showFatalLogButton.Text = "Только FATAL";
         }
 
         private void ShowInfoLogButton_Click(object sender, EventArgs e)
         {
             _logger.WriteInfoLogInLogBox();
+            showLogButton.Text = "Показать весь лог";
+            showInfoLogButton.Text = "Обновить";
+            showErrorLogButton.Text = "Только ERROR";
+            showFatalLogButton.Text = "Только FATAL";
         }
 
         private void ShowErrorLogButton_Click(object sender, EventArgs e)
         {
             _logger.WriteErrorLogInLogBox();
+            showLogButton.Text = "Показать весь лог";
+            showInfoLogButton.Text = "Только INFO";
+            showErrorLogButton.Text = "Обновить";
+            showFatalLogButton.Text = "Только FATAL";
         }
 
         private void ShowFatalLogButton_Click(object sender, EventArgs e)
         {
             _logger.WriteFatalLogInLogBox();
+            showLogButton.Text = "Показать весь лог";
+            showInfoLogButton.Text = "Только INFO";
+            showErrorLogButton.Text = "Только ERROR";
+            showFatalLogButton.Text = "Обновить";
         }
 
         private void LogForm_FormClosed(object sender, FormClosedEventArgs e)
